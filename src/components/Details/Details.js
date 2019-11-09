015a357d46666ef57af4257d5462476652ad9d83
Brooks Geenen <brgeen@gmail.com>
2019-11-09 15:54:12 -0600

    add back to list button, links user back to Movies component

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Details extends Component {
 
     render() {
         return (
             <div className="App">
+                <Link to="/"><button>Back To List</button></Link>
                 {this.props.reduxState.movieDetails.map(movie =>
                     <div key={movie.id} className="movie-container">
                         <div className="movie-title-image-container">
